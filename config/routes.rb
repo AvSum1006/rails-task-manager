b3c9b47ce77daa76858f38940334894298219716
adrien hucq <hucq.adrien@outlook.com>
2023-08-12 05:06:58 +0200

    Finished exercise

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,25 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :tasks
+  # # View all tasks list
+  # get '/tasks', to: 'tasks#index'
+
+  # # Add new task
+  # # Go to form
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # # post the new task
+  # post '/tasks', to: 'tasks#create'
+
+  # # Read one
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  # # Update
+  # # Get the task
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # # Patch edit
+  # patch '/tasks/:id', to: 'tasks#update'
+
+  # # Delete task
+  # delete '/tasks/:id', to: 'tasks#destroy'
 end
